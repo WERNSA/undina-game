98a99d4e3d76d30dd384f9b9493844213eec3051
bonillab <nestorbonillabrenes@gmail.com>
2025-06-12 17:00:12 -0600

    update tween on ObstacleRace

--- Scenes/Games/ObstacleRace/Nivel1/ObstacleRace.gd
+++ Scenes/Games/ObstacleRace/Nivel1/ObstacleRace.gd
@@ -2,24 +2,21 @@ extends Node2D
 
 @export var Island1: PackedScene
 @export var Island2: PackedScene
-
 @export var Trash1: PackedScene
 @export var Trash2: PackedScene
 @export var Trash3: PackedScene
 @export var Trash4: PackedScene
-@onready var island_position = Vector2.ZERO
+
+@onready var island_position = Vector2(3600, 1550)
 @onready var top_position = 250
 @onready var bottom_position = 1550
 @onready var tries = 3
 var TRASH_COUNT = 0
-var game_over : bool = false
+var game_over: bool = false
 
-# Called when the node enters the scene tree for the first time.
 func _ready():
 	$Spawn/IslandTimer.start()
 	$Spawn/IslandSpeedTimer.start()
-	island_position.y = bottom_position
-	island_position.x = 3600
 	$GameOver/CenterContainer/HBoxContainer/BtnTry.connect("pressed", Callable(self, "_on_try_again"))
 	$Win/CenterContainer/HBoxContainer/BtnTry.connect("pressed", Callable(self, "_on_try_again"))
 	$Contador.set_count(str(TRASH_COUNT))
@@ -29,9 +26,9 @@ func _process(_delta):
 	$Timer/VBoxContainer/Timer/Label.text = Global.get_timer($Timer/Timer.time_left)
 
 func _on_IslandTimer_timeout():
-	get_node("Spawn/IslandPath/IslandSpawn").progress = randi()
+	$Spawn/IslandPath/IslandSpawn.progress = randi()
 	var island_number = Global.random_int(1, 2)
-	var island
+	var island: Node2D
 	if island_number == 1:
 		island = Island1.instantiate()
 	else:
@@ -39,20 +36,22 @@ func _on_IslandTimer_timeout():
 	add_child(island)
 	island.position = island_position
 	spawn_trash()
+
 	if island_position.y == top_position:
 		island.rotate_object(90)
 		island_position.y = bottom_position
 	else:
 		island.rotate_object(-90)
 		island_position.y = top_position
+
 	$Spawn/IslandTimer.wait_time = Global.random_float(2, 3)
 	$Spawn/IslandTimer.start()
 
 func spawn_trash():
 	var is_spawn = Global.random_int(0, 2)
-	if is_spawn == 1 or is_spawn == 2:
+	if is_spawn in [1, 2]:
 		var trash_number = Global.random_int(1, 4)
-		var trash
+		var trash: Node2D
 		match trash_number:
 			1:
 				trash = Trash1.instantiate()
@@ -66,18 +65,13 @@ func spawn_trash():
 				trash = Trash1.instantiate()
 		add_child(trash)
 		trash.position = island_position
-		var position_x = Global.random_int(1, 4)
-		match position_x:
-			1:
-				trash.position.x += 800
-			2:
-				trash.position.x += 1000
-			3:
-				trash.position.x += 1200
-			4:
-				trash.position.x += 1400
-			_:
-				trash.position.x += 1400
+		trash.position.x += 800 + (Global.random_int(0, 3) * 200)
+
+func move_turtle_to(target_position: Vector2, duration: float = 1.0):
+	var tween = create_tween()
+	tween.tween_property($Characters/Turtle, "position", target_position, duration)\
+		.set_trans(Tween.TRANS_LINEAR)\
+		.set_ease(Tween.EASE_IN_OUT)
 
 func reduce_distance():
 	TRASH_COUNT += 10
@@ -85,40 +79,24 @@ func reduce_distance():
 	tries = 3
 	$Sounds/GetSound.play()
 	var new_position = $Characters/Turtle.position.x - 400
-	$Characters/Tween.interpolate_property(
-		$Characters/Turtle, "position",$Characters/Turtle.position,
-		Vector2(new_position, $Characters/Turtle.position.y), 1,
-		Tween.TRANS_LINEAR, Tween.EASE_IN_OUT
-	)
-	$Characters/Tween.start()
+	move_turtle_to(Vector2(new_position, $Characters/Turtle.position.y))
 
 func add_distance():
-	if $Characters/Turtle.position.x >= 2700:
+	var current_x = $Characters/Turtle.position.x
+	if current_x >= 2700:
 		tries -= 1
 		if tries == 0:
-			var new_position = $Characters/Turtle.position.x + 600
-			$Characters/Tween.interpolate_property(
-				$Characters/Turtle, "position",$Characters/Turtle.position,
-				Vector2(new_position, $Characters/Turtle.position.y), 1,
-				Tween.TRANS_LINEAR, Tween.EASE_IN_OUT
-			)
-			$Characters/Tween.start()
+			move_turtle_to(Vector2(current_x + 600, $Characters/Turtle.position.y))
 			_game_over()
 	else:
-		var new_position = $Characters/Turtle.position.x + 400
-		$Characters/Tween.interpolate_property(
-			$Characters/Turtle, "position",$Characters/Turtle.position,
-			Vector2(new_position, $Characters/Turtle.position.y), 1,
-			Tween.TRANS_LINEAR, Tween.EASE_IN_OUT
-		)
-		$Characters/Tween.start()
+		move_turtle_to(Vector2(current_x + 400, $Characters/Turtle.position.y))
 
 func _on_IslandSpeedTimer_timeout():
 	Global.min_enemy_speed += 50
 	Global.max_enemy_speed += 50
 	if Global.min_enemy_speed > 500:
 		$Spawn/IslandSpeedTimer.stop()
-		print("Velocidad maxima alcanzada")
+		print("Velocidad máxima alcanzada")
 	print("Velocidad enemiga aumentada")
 
 func win():
@@ -155,11 +133,5 @@ func _on_StepTimer_timeout():
 func _on_Timer_timeout():
 	$Timer/StepTimer.stop()
 	tries = 0
-	var new_position = 3400
-	$Characters/Tween.interpolate_property(
-		$Characters/Turtle, "position",$Characters/Turtle.position,
-		Vector2(new_position, $Characters/Turtle.position.y), 2,
-		Tween.TRANS_LINEAR, Tween.EASE_IN_OUT
-	)
-	$Characters/Tween.start()
+	move_turtle_to(Vector2(3400, $Characters/Turtle.position.y), 2.0)
 	_game_over()
